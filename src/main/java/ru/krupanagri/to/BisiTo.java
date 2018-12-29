613b211d1f89f12046fe24705c808b97c11fd62e
Pavel <kareglas83@mail.ru>
2018-12-29 10:34:10 +0300

    add product.jsp, profile.jsp. Work with DB.

---
+++ src/main/java/ru/krupanagri/to/BisiTo.java
@@ -0,0 +1,70 @@
+package ru.krupanagri.to;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class BisiTo extends BaseTo {
+
+    private LocalDateTime dateTime;
+
+    private String description;
+
+    private int calories;
+
+    private boolean excess;
+
+    public BisiTo() {
+    }
+
+    public BisiTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+        super(id);
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.excess = excess;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExcess() {
+        return excess;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BisiTo that = (BisiTo) o;
+        return calories == that.calories &&
+                excess == that.excess &&
+                Objects.equals(id, that.id) &&
+                Objects.equals(dateTime, that.dateTime) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, dateTime, description, calories, excess);
+    }
+
+    @Override
+    public String toString() {
+        return "BisiTo{" +
+                "id=" + id +
+                ", dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                ", excess=" + excess +
+                '}';
+    }
+}
